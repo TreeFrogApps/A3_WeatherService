faf98f7d5c0973025ceb3b00143fc5dfc5c7fadf
markkeen <chugnut>
2016-01-18 02:34:53 +0000

    Basic unit testing added

--- app/src/androidTest/java/com/treefrogapps/a3_weatherservice/tests/WeatherActivityTests.java
+++ app/src/androidTest/java/com/treefrogapps/a3_weatherservice/tests/WeatherActivityTests.java
@@ -3,6 +3,7 @@ package com.treefrogapps.a3_weatherservice.tests;
 import android.support.v7.widget.RecyclerView;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.robotium.solo.Condition;
 import com.robotium.solo.Solo;
@@ -31,41 +32,47 @@ public class WeatherActivityTests extends ActivityInstrumentationTestCase2 {
         mSolo.finishOpenedActivities();
     }
 
-    public void testRun(){
+    public void testRunValidCities(){
+
+        String[] citiesToTest = new String[]
+                {"London", "New York", "Milan", "Mexico City", "Edinburgh", "Madrid", "Riga", "Moscow"};
 
         mSolo.assertCurrentActivity("Wrong Activity", WeatherActivity.class);
 
         assertTrue("Activity failed to load", mSolo.waitForActivity(WeatherActivity.class, 10000));
 
-        mSolo.clickOnView(mSolo.getView(R.id.fabButton));
 
-        assertTrue("Window dialog not open", mSolo.waitForDialogToOpen(1000));
+        for (final String city : citiesToTest){
 
-        mSolo.setActivityOrientation(Solo.LANDSCAPE);
+            mSolo.clickOnView(mSolo.getView(R.id.fabButton));
 
-        mSolo.setActivityOrientation(Solo.PORTRAIT);
+            assertTrue("Window dialog not open", mSolo.waitForDialogToOpen(1000));
 
-        mSolo.enterText((EditText) mSolo.getView(R.id.dialogCityEditText), "London");
+            mSolo.setActivityOrientation(Solo.LANDSCAPE);
 
-        mSolo.setActivityOrientation(Solo.LANDSCAPE);
+            mSolo.enterText((EditText) mSolo.getView(R.id.dialogCityEditText), city);
 
-        mSolo.setActivityOrientation(Solo.PORTRAIT);
+            mSolo.setActivityOrientation(Solo.PORTRAIT);
 
-        assertTrue(mSolo.searchText("London"));
+            assertTrue(mSolo.searchText(city));
 
-        mSolo.clickOnView(mSolo.getView(R.id.dialogASyncButton));
-
-
-        final RecyclerView recyclerView = (RecyclerView) mSolo.getView(R.id.recyclerView);
-
-        Condition condition = new Condition() {
-            @Override
-            public boolean isSatisfied() {
-                return recyclerView.getAdapter().getItemCount() > 0;
-            }
-        };
+            mSolo.clickOnView(mSolo.getView(R.id.dialogASyncButton));
 
 
-        assertTrue("Problem with loading the weather", mSolo.waitForCondition(condition, 10000));
+            final RecyclerView recyclerView = (RecyclerView) mSolo.getView(R.id.recyclerView);
+            final TextView textView = (TextView) mSolo.getView(R.id.currentTextViewCityName);
+
+            Condition condition = new Condition() {
+                @Override
+                public boolean isSatisfied() {
+                    return recyclerView.getAdapter().getItemCount() > 0 &&
+                            textView.getText().toString().equals(city);
+                }
+            };
+
+            assertTrue("Problem with loading the weather for " + city, mSolo.waitForCondition(condition, 10000));
+        }
+
+
     }
 }
